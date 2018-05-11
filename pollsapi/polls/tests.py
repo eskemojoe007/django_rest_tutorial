be8d43b6e783f770771c7194687d5971310809be
eskemojoe007 <David.Folkner@gmail.com>
2018-05-10 23:34:48 -0400

    Added simple VUE.js template

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+# from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from . import apiviews
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.factory = APIRequestFactory()
+        self.view = apiviews.PollViewSet.as_view({'get':'list'})
+        self.uri = '/polls/'
+        self.user = self.setup_user()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='test@test.come',
+            password='test_pass'
+        )
+
+    def test_list(self):
+        request = self.factory.get(self.uri)
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(
+            response.status_code,200,
+            'Expected Response code 200, recieved {} instead.'
+            .format(response.status_code))
+    def test_list2(self):
+        self.client.login(username='test',password='test_pass')
+        response = self.client.get(self.uri)
+        self.assertEqual(
+            response.status_code,200,
+            'Expected Response code 200, recieved {} instead.'
+            .format(response.status_code))
+    def test_create(self):
+        self.client.login(username='test',password='test_pass')
+        params = {
+            'question':'Test question?',
+            'created_by':1
+        }
+
+        response = self.client.post(self.uri,params)
+        self.assertEqual(
+            response.status_code,201,
+            'Expected Response code 200, recieved {} instead.'
+            .format(response.status_code))
--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -2,20 +2,19 @@ from django.urls import path, include
 # from .apiviews import PollList, PollDetail, ChoiceList, CreateVote, PollViewSet
 from .apiviews import  ChoiceList, CreateVote, PollViewSet, UserCreate, LoginView
 from rest_framework.routers import DefaultRouter
+from django.views.generic import TemplateView
 
 # For view sets the default router attaches the proper function to the methods
 router = DefaultRouter()
 router.register('polls',PollViewSet,base_name='poll')
 router.register('users',UserCreate,base_name='users')
 
-
+app_name = 'polls'
 urlpatterns = [
     path('login/',LoginView.as_view(),name='login'),
-    # path('polls/',PollList.as_view(),name='polls_list'),
-    # path('polls/<int:pk>/',PollDetail.as_view(),name='polls_detail'),
     path('polls/<int:pk>/choices/',ChoiceList.as_view(),name='choice_list'),
-    path('polls/<int:pk>/choices/<int:choice_pk>/vote/',CreateVote.as_view(),name='create_vote'),
-    # path('users/',UserCreate.as_view(),name='user_create'),
+    path('polls/<int:pk>/choices/<int:choice_pk>/vote/',
+        CreateVote.as_view(),name='create_vote'),
+    path('',TemplateView.as_view(template_name='polls/index.html')),
 ]
-
 urlpatterns += router.urls
